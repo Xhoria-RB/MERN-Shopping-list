6a4cc779bd923b3cfd71a6321dd0022a22f1cb1b
Ricardo Bibieca <shirotokuro3715@gmail.com>
2018-09-23 20:14:26 -0400

    Fixed compatibility

--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,8 @@
-import {createStore, applyMiddleware, compose} from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer  from './reducers';//Since the rootReducer is in a index.js isn't needed to be added here /reducers/index.js
+import rootReducer from './reducers'; //Since the rootReducer is in a index.js isn't needed to be added here /reducers/index.js
 
-//Represents our inital state 
+//Represents our inital state
 const initialState = {};
 
 //Any middleware to use will be placed inside an array
@@ -14,9 +14,23 @@ const middleware = [thunk];
  * ...middleware will bring the content of middleware[]
  * window.__REDUX_DEVTOOLS_EXTENSION__ is from the redux devtools extension
  */
-const store = createStore(rootReducer, initialState, compose(
-  applyMiddleware(...middleware),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-));
+const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeEnhancers(applyMiddleware(...middleware))
+);
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   compose(
+//     applyMiddleware(...middleware),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
 
 export default store;
+//In my store.js I changed the createstore to:
+
+// const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+// const store = createStore(rootReducer, initialState, composeEnhancers(applyMiddleware(...middleware)));
